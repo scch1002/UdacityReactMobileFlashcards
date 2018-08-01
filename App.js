c8c328b73f366afbea5aff641f8cbe7f9c451419
Schwan <50006@icf.com>
2018-08-01 10:40:44 -0500

    Added initial tab navigation for desks

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from 'react-navigation';
+
+const DeckViewTab = createMaterialTopTabNavigator({
+  Decks: {
+    screen: () => (<Text>Decks</Text>),
+    navigationOptions: {
+      tabBarLabel: 'Decks'
+    },
+  },
+  AddDeck: {
+    screen: () => (<Text>AddDeck</Text>),
+    navigationOptions: {
+      tabBarLabel: ' Add Deck'
+    }
+  }
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <DeckViewTab />
       </View>
     );
   }
@@ -17,7 +31,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
