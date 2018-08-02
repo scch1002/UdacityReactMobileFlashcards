7ea95730753927190e015e844c0b1dcb665313d9
Schwan <50006@icf.com>
2018-08-02 18:21:37 -0500

    Created initial check for storage setup

--- appWrapper.js
+++ appWrapper.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { initialStorageCheck } from './utils/storageHelpers'
 import { retreiveLoadDecks } from './actions/decks'
 
 class AppWrapper extends Component {
-    componentDidMount() {
-        this.props.dispatch(retreiveLoadDecks())
+    async componentDidMount() {
+        await initialStorageCheck()
+        await this.props.dispatch(retreiveLoadDecks)
     }
     render() {
         return this.props.children
