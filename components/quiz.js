90d202e2213ee4da0beb600e1498d9c5a5ac2af2
Schwan <50006@icf.com>
2018-08-08 16:32:27 -0500

    Added more styling

--- components/quiz.js
+++ components/quiz.js
@@ -52,8 +52,8 @@ class Quiz extends Component {
             return (
                 <View style={styles.container}>
                     <View style={{flex: 1, justifyContent: 'center'}}>
-                        <Text>Quiz Complete</Text>
-                        <Text>{`Score: ${this.state.score}/${deck.cards.length}`}</Text>
+                        <Text style={{ textAlign: 'center', margin: 10, fontWeight: 'bold', fontSize: 35 }}>Quiz Complete</Text>
+                        <Text style={{ textAlign: 'center', margin: 10, fontWeight: 'bold', fontSize: 35 }}>{`Score: ${this.state.score}/${deck.cards.length}`}</Text>
                     </View>
                     <View style={{flex: 1, justifyContent: 'flex-end'}}>
                         <Button title="Restart Quiz" onPress={this.restartQuiz}></Button>
@@ -67,8 +67,8 @@ class Quiz extends Component {
             <View style={styles.container}>
                 <View style={{flex: 1, justifyContent: 'center'}}>
                     { this.state.showAnswer ? 
-                        <Text>{card.text}</Text> :
-                        <Text>{card.answer}</Text>
+                        <Text style={{ textAlign: 'center', margin: 10, fontWeight: 'bold', fontSize: 35 }}>{card.text}</Text> :
+                        <Text style={{ textAlign: 'center', margin: 10, fontWeight: 'bold', fontSize: 35 }}>{card.answer}</Text>
                     }
                 </View>
                 <View style={{flex: 1, justifyContent: 'flex-end'}}>
